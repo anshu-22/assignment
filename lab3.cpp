ce61359019c00b3652d19dd2eb488695c5a1fca5
anshu-22 <42144487+anshu-22@users.noreply.github.com>
2018-08-16 11:19:23 -0400

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,21 @@
+//include library
+#include <iostream>
+using namespace std;
+//main function
+int main() {
+int sizeint; // declaration
+sizeint = sizeof (int); //initalization
+cout << "the size of integer is " << sizeint << endl; // prints on terminal
+int sizefloat;
+sizefloat = sizeof (float);
+cout << "the size of float is " << sizefloat << "bytes" << endl;
+int sizedouble;
+sizedouble =sizeof (double);
+cout << "the size of double is" << sizedouble << "bytes" << endl;
+int sizechar;
+sizechar =sizeof (char);
+cout << "the size of character is " << sizechar << "bytes" << endl;int sizeboolean;
+sizeboolean = sizeof (bool);
+cout << "the size of boolean is" << sizeboolean << "bytes" << endl;
+return 0;
+}
