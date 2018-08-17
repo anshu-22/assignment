bc9589f83d236915c04a729f55fa7206e46ea271
anshu-22 <42144487+anshu-22@users.noreply.github.com>
2018-08-17 14:29:24 +0530

    Add files via upload

---
+++ lab3q2.cpp
@@ -0,0 +1,38 @@
+// operating with variables
+
+#include <iostream>
+using namespace std;
+
+int main (){
+// declaring two integer variables
+int a, b;
+//initializing values to the variables
+a=9;
+b=3;
+//finding the sum of a and b
+int sum=a+b;
+//displaying the sum of a and b
+;cout << "Sum of a and b =" <<sum <<endl;
+
+//finding the difference of a and b
+int diff=a-b
+//displaying the difference of a and b
+;cout << "difference of a and b =" <<diff <<endl;
+
+//finding the multiplication of a and b
+int mul=a*b
+//displaying the multiplication of a and b
+;cout << "multiplication of a and b =" <<mul <<endl;
+
+//dividing a and b
+int div=a/b
+//displaying the division of a and b
+;cout << "dividing b by a =" <<div <<endl;
+
+//finding the modulus of a and b
+int m=b%a;
+//displaying modulus of a and b
+;cout << "The modulus of b over a =" <<m <<endl;
+//return any integer value to int function 
+return 0;
+}
