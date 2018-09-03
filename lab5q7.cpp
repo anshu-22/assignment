43c3c6ed9a339fdc66489241c32f9bc41b05bbe5
anshu-22 <42144487+anshu-22@users.noreply.github.com>
2018-09-03 13:20:31 +0530

    Add files via upload

---
+++ lab5q7.cpp
@@ -0,0 +1,20 @@
+//C program to print natural nos. from 1 to n
+#include<iostream>
+using namespace std;
+int main()
+{
+//declaring variables
+float num,n;
+//print all natural nos. from 1 to 100
+cout<<"All natural no. from 1 to 100 :\n\n";
+
+n=1;
+num=100;
+//defining the loop
+while(n<=num)
+{
+cout<<n;
+n++;
+}
+return 0;
+}
